70355861060b42be107b952524978ff1b19862b9
Graham Mainwaring <graham@mhn.org>
2024-12-10 02:18:29 -0500

    Day 10

---
+++ cmd/10/p10.go
@@ -0,0 +1,86 @@
+package main
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/ghjm/advent_utils"
+)
+
+type data struct {
+	board *utils.StdBoard
+}
+
+func (d *data) score(start utils.StdPoint) int {
+	open := []utils.StdPoint{start}
+	visited := make(map[utils.StdPoint]struct{})
+	summits := make(map[utils.StdPoint]struct{})
+	for len(open) > 0 {
+		p := open[0]
+		open = open[1:]
+		if _, ok := visited[p]; ok {
+			continue
+		}
+		visited[p] = struct{}{}
+		pv := d.board.Get(p)
+		for _, c := range d.board.Cardinals(p) {
+			if _, ok := visited[c]; ok {
+				continue
+			}
+			cv := d.board.Get(c)
+			if cv == pv+1 {
+				if cv == '9' {
+					summits[c] = struct{}{}
+				} else {
+					open = append(open, c)
+				}
+			}
+		}
+	}
+	return len(summits)
+}
+
+func (d *data) rating(p utils.StdPoint) int {
+	pv := d.board.Get(p)
+	if pv == '9' {
+		return 1
+	}
+	sum := 0
+	for _, c := range d.board.Cardinals(p) {
+		cv := d.board.Get(c)
+		if cv == pv+1 {
+			sum += d.rating(c)
+		}
+	}
+	return sum
+}
+
+func run() error {
+	d := data{
+		board: utils.NewStdBoard(&utils.FlatBoard{}),
+	}
+	err := d.board.FromFile("input10.txt")
+	if err != nil {
+		return err
+	}
+	part1 := 0
+	part2 := 0
+	d.board.Iterate(func(p utils.Point[int], v rune) bool {
+		if v == '0' {
+			part1 += d.score(p)
+			part2 += d.rating(p)
+		}
+		return true
+	})
+	fmt.Printf("Part 1: %d\n", part1)
+	fmt.Printf("Part 2: %d\n", part2)
+	return nil
+}
+
+func main() {
+	err := run()
+	if err != nil {
+		fmt.Printf("Error: %s\n", err)
+		os.Exit(1)
+	}
+}
--- go.mod
+++ go.mod
@@ -4,4 +4,4 @@ go 1.23.3
 
 require golang.org/x/exp v0.0.0-20241204233417-43b7b7cde48d
 
-require github.com/ghjm/advent_utils v0.0.0-20241208075412-9a903fd47f8c // indirect
+require github.com/ghjm/advent_utils v0.0.0-20241210071813-03e26bc988d4 // indirect
